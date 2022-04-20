c8db94736175965f0ea2876edc00d044b58e44cb
Zarema Tassyrova <zarema.tsr@gmail.com>
2022-04-20 14:02:49 -0400

    another change

---
+++ spiderMan.js
@@ -0,0 +1,24 @@
+const spiderMan = [
+    {
+    id: "01",
+    name: "Tobey Maguire",
+    img: "https://wallpapercave.com/wp/wp8303736.jpg",
+    movies: [ ]
+    },
+    {
+        id: "02",
+        name: "Andrew Garfield",
+        img:"https://i.pinimg.com/736x/07/fe/79/07fe79e101e7af4a3f07df3388f929f2.jpg",
+        movies:[]
+    },
+    {
+        id: "03",
+        name: "Tom Holland",
+        img: fhj,
+        movies: []
+
+    }
+
+];
+
+module.exports = spiderMan
