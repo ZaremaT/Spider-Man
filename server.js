9288ddbadb5fc7d4776d52e7c9e6911bd0ee8d69
anpavlov11 <apavlo11@gmail.com>
2022-04-20 14:53:06 -0500

    added path for index

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 4000
 
+const spiderMan = require("./spiderMan.ejs");
+
+app.get("/", (req, res) => {
+    const context = {spiderMan};
+    res.render("index.ejs", context);
+})
+
 app.listen(PORT, () => console.log( `Listening on ${PORT} `))
